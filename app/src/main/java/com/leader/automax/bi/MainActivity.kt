d318049ffdd64887c6a46ac88e98c12730a47a35
s.nabil <s.nabil@lesoft.com>
2021-02-05 21:40:16 +0530

    home activity

--- app/src/main/java/com/leader/automax/bi/MainActivity.kt
+++ app/src/main/java/com/leader/automax/bi/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.leader.automax.bi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        AlertDialog.Builder(this)
+            .setMessage(R.string.exit_prompt)
+            .setPositiveButton(android.R.string.ok) { _: DialogInterface?, _: Int -> finishAffinity() }
+            .setNegativeButton(android.R.string.cancel, null)
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .show()
     }
 }
