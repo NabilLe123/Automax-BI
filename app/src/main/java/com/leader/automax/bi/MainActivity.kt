6a661b117600a106b71ae7eeeb986655f6184c95
s.nabil <s.nabil@lesoft.com>
2021-02-08 15:01:39 +0530

    custom tab intent to load url

--- app/src/main/java/com/leader/automax/bi/MainActivity.kt
+++ app/src/main/java/com/leader/automax/bi/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.leader.automax.bi
 
 import android.content.DialogInterface
-import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
+    //private val url: String = "https://www.webpagetest.org/"
+
+    private val url: String =
+        "https://Eamana%5CAutomaxsup:3PM940%40l3%40d3r@940bi.eamana.gov.sa/reports/powerbi/IncidentDashboardCovid19MobileAR?rs:embed=true"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,8 +20,13 @@ class MainActivity : AppCompatActivity() {
 
         val cvIncidents = findViewById<CardView>(R.id.cv_incidents)
         cvIncidents.setOnClickListener {
-            val intent = Intent(this, IncidentsActivity::class.java)
-            startActivity(intent)
+            //val intent = Intent(this, IncidentsActivity::class.java)
+            //startActivity(intent)
+
+            val builder = CustomTabsIntent.Builder()
+            builder.setUrlBarHidingEnabled(false)
+            val customTabsIntent = builder.build()
+            customTabsIntent.launchUrl(this, Uri.parse(url))
         }
     }
 
